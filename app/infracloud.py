b3ac2a452451c6d7dc7e2381dfa022cdbaab0dd3
Naman Jhanjhari <naman.jhanjhari@visiblealpha.com>
2020-01-20 04:31:39 -0800

    COnfig Bug Fix

--- app/infracloud.py
+++ app/infracloud.py
@@ -6,10 +6,10 @@ import ujson
 from configparser import ConfigParser
 
 class Url():
-    config = ConfigParser()
-    config.read('config.ini')
 
     def on_get(self, req, resp):
+        config = ConfigParser()
+        config.read('config.ini')
         req_params = req.params
         conn = redis.Redis(config.get('redis', 'host'),db=3)
         original_url = conn.get(req_params["short_url"])
@@ -19,11 +19,13 @@ class Url():
         resp.body = resp_json
 
     def on_post(self,req, resp):
+        config = ConfigParser()
+        config.read('config.ini')
         url = req.get_param("original_url", required=True)
-        print (url)
         shortened_url = "b."+str(randint(1000,9999))+url[0:5]
         conn = redis.Redis(config.get('redis', 'host'),db=3)
         conn.set(shortened_url,url)
+        resp.body=shortened_url
 
 
 
@@ -31,5 +33,5 @@ api = falcon.API()
 api.add_route('/shorten_url', Url())
 api.add_route('/get_url',Url())
 
-#if __name__ == '__main__':
-#    run_simple('localhost', 1234, api, use_reloader=True, passthrough_errors=True)
+if __name__ == '__main__':
+   run_simple('localhost', 1234, api, use_reloader=True, passthrough_errors=True)
